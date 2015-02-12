c18028993689f5c40f785a233e36f33e9cc2dc51
Daekyu Lee <dklee@yidigun.com>
2015-02-12 12:36:15 +0900

    autoload feature

---
+++ src/autoload.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * autoload.php
+ * Setup classpath and autoloader
+ */
+
+/**
+ * Wheather print debug informations when autoloading.
+ */
+if (!isset($GLOBALS['autoload_debug']))
+	$GLOBALS['autoload_debug'] = false;
+
+/**
+ * Global array to store classpath.
+ */
+$GLOBALS['YIDIGUN_AUOTLOAD_PATH'] = array();
+
+/**
+ * Setup classpath for autoloader function.
+ */
+function autoload_classpath($basedir, $dirs = null) {
+
+	$paths = array($basedir);
+
+	if ($dirs) {
+		if (is_array($dirs)) {
+			foreach ($dirs as $dir)
+				$paths[] = (strpos($dir, DIRECTORY_SEPARATOR) === 0)?
+							$dir: $basedir . DIRECTORY_SEPARATOR . $dir;
+		}
+		else {
+			$paths[] = (strpos($dirs, DIRECTORY_SEPARATOR) === 0)?
+						$dirs: $basedir . DIRECTORY_SEPARATOR . $dirs;
+		}
+	}
+
+	$GLOBALS['YIDIGUN_AUOTLOAD_PATH'] = $paths;
+}
+
+/*
+ * Register autoloader function and start autoloading.
+ */
+spl_autoload_register(function($class){
+
+	$file = str_replace("_", DIRECTORY_SEPARATOR,
+				str_replace("\\", DIRECTORY_SEPARATOR, $class)) . ".php";
+
+	$file_exists = false;
+	if (is_array($GLOBALS['YIDIGUN_AUOTLOAD_PATH'])) {
+		foreach ($GLOBALS['YIDIGUN_AUOTLOAD_PATH'] as $dir) {
+			// check name as-is
+			$path = $dir . DIRECTORY_SEPARATOR . $file;
+			if (file_exists($path)) {
+				$file_exists = true;
+				include_once($path);
+				if (class_exists($class))
+					break;
+			}
+			// force name to lower case and try again
+			$path = $dir . DIRECTORY_SEPARATOR . strtolower($file);
+			if (file_exists($path)) {
+				$file_exists = true;
+				include_once($path);
+				if (class_exists($class))
+					break;
+			}
+		}
+	}
+
+	if ($GLOBALS['autoload_debug']) {
+		echo "[autoloader]: {$class} => {$file}";
+		echo ", file " . (($file_exists)? "exists": "not exists");
+		echo ", class " . ((class_exists($class))? "loaded": "not loaded");
+		echo ".\n";
+	}
+});
